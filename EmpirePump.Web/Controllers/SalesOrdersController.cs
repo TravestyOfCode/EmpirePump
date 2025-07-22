fac18d21fb0d8d0a3533be0401ecbf296b49bf86
travis@travestyofcode.com <travis@travestyofcode.com>
2025-07-22 17:32:46 -0400

    Adds SalesOrder controller.

---
+++ EmpirePump.Web/Controllers/SalesOrdersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmpirePump.Web.Controllers;
+
+[ApiController]
+public class SalesOrdersController(ILogger<SalesOrdersController> logger) : ControllerBase
+{
+    [HttpPost("[controller]/[action]")]
+    public IActionResult Create([FromBody] string? notes)
+    {
+        logger.LogDebug(notes);
+
+        return Ok();
+    }
+}

59b6323e55190f8b40e17c3cfae9c7134c1c9f1e
travis@travestyofcode.com <travis@travestyofcode.com>
2025-07-22 15:55:39 -0400

    Add project files.

---
+++ EmpirePump.Web/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmpirePump.Web.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(System.Reflection.Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ EmpirePump.Web/Data/AppUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EmpirePump.Web.Data;
+
+public class AppUser : IdentityUser
+{
+
+}

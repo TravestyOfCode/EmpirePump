9f3417e06923f49a634141173713ac57c5f7114f
travis@travestyofcode.com <travis@travestyofcode.com>
2025-08-09 03:46:43 -0400

    Adds view and model for matched transactions.

---
+++ EmpirePump.Web/Models/Reconcile/MatchedTransactionModel.cs
@@ -0,0 +1,7 @@
+﻿namespace EmpirePump.Web.Models.Reconcile;
+
+public class MatchedTransactionModel
+{
+    public List<Transaction> BankTransactions { get; set; } = [];
+    public List<Transaction> QBTransactions { get; set; } = [];
+}

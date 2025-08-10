eec0489e812ed853cee50ab14d3192e5ede6ff59
travis@travestyofcode.com <travis@travestyofcode.com>
2025-08-10 13:54:06 -0400

    Adds AccountQueryRq.

---
+++ EmpirePump.Web/QBSDK/Queries/AccountQueryRq.cs
@@ -0,0 +1,61 @@
+﻿using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace EmpirePump.Web.QBSDK;
+
+public class AccountQueryRq : ListQueryRq
+{
+    /// <summary>
+    /// Filters according to the object's Name.
+    /// </summary>
+    public NameFilter? NameFilter { get; set; }
+
+    /// <summary>
+    /// Filters according to the object's Name.
+    /// </summary>
+    public NameRangeFilter? NameRangeFilter { get; set; }
+
+    /// <summary>
+    /// A list of QuickBooks account types to filter.
+    /// </summary>
+    public List<AccountType>? AccountType { get; set; }
+
+    /// <summary>
+    /// Filters by the specified currency. 
+    /// </summary>
+    public ListFilter? CurrencyFilter { get; set; }
+
+    /// <summary>
+    /// If the query was successful, this will contain a list of objects that
+    /// match the queries filters. If no matches were found (StatusCode 500)
+    /// the list will be empty. If there was an error with the query, the list
+    /// will be null.
+    /// </summary>
+    [XmlElement("AccountRet")]
+    public List<Account>? Results { get; set; }
+
+    /// <summary>
+    /// Converts the request into an XElement.
+    /// </summary>
+    /// <param name="context">The QBContext used to determine which fields are suppported.</param>
+    /// <returns>An XElement representing the request.</returns>    
+    public override XElement ToXElement(QBContext? context)
+    {
+        context ??= new QBContext();
+
+        return new XElement(nameof(AccountQueryRq))
+            .AddAttributeIf(context.Supports(QBEdition.Any, 4, 0), metaData)
+            .AddElement(ListID)
+            .AddElement(FullName)
+            .AddElement(MaxReturned)
+            .AddElement(ActiveStatus)
+            .AddElement(FromModifiedDate)
+            .AddElement(ToModifiedDate)
+            .AddElement(NameFilter)
+            .AddElement(NameRangeFilter)
+            .AddElement(AccountType)
+            .AddElement(CurrencyFilter)
+            .AddElement(IncludeRetElement)
+            .AddElement(OwnerID);
+    }
+}

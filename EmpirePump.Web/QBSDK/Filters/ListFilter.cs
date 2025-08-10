41850f00dc2d37f55646d4445ec53e8e61457657
travis@travestyofcode.com <travis@travestyofcode.com>
2025-08-10 13:53:50 -0400

    Adds filter for Lists.

---
+++ EmpirePump.Web/QBSDK/Filters/ListFilter.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Linq;
+
+namespace EmpirePump.Web.QBSDK;
+
+public class ListFilter : IToXElement
+{
+    public List<string>? ListID { get; set; }
+
+    public List<string>? FullName { get; set; }
+
+    public XElement ToXElement(string name = nameof(ListFilter))
+    {
+        return new XElement(name)
+            .AddElement(ListID)
+            .AddElement(FullName);
+    }
+}

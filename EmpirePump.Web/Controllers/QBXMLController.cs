81073604e16ed5edd96e8bf9569bf6deea5907c1
travis@travestyofcode.com <travis@travestyofcode.com>
2025-08-10 19:56:43 -0400

    Adds controller to test QBXML.

---
+++ EmpirePump.Web/Controllers/QBXMLController.cs
@@ -0,0 +1,17 @@
+﻿using EmpirePump.Web.QBSDK;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmpirePump.Web.Controllers;
+
+public class QBXMLController(QBConnection connection) : Controller
+{
+    public IActionResult Index()
+    {
+        var qbxml = new QBXML();
+        qbxml.AddRequest(AccountQueryRq.GetReconcilableAccounts());
+
+        qbxml.ProcessRequests(connection);
+
+        return Ok(qbxml.Requests.FirstOrDefault()?.StatusMessage);
+    }
+}

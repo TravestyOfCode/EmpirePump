856ce9ae4fdb40295389828c9d4f3b3bb1968f5d
travis@travestyofcode.com <travis@travestyofcode.com>
2025-08-10 14:51:20 -0400

    Clears out XMLElement attributes for classes not  implemented yet.

--- EmpirePump.Web/QBSDK/Types/QBXMLRequestList.cs
+++ EmpirePump.Web/QBSDK/Types/QBXMLRequestList.cs
@@ -6,9 +6,9 @@ namespace EmpirePump.Web.QBSDK;
 public class QBXMLRequestList
 {
     [XmlElement("AccountQueryRs", typeof(AccountQueryRq))]
-    [XmlElement("AccountModRs", typeof(AccountModRq))]
-    [XmlElement("AccountAddRs", typeof(AccountAddRq))]
-    [XmlElement("CustomerQueryRs", typeof(CustomerQueryRq))]
+    //[XmlElement("AccountModRs", typeof(AccountModRq))]
+    //[XmlElement("AccountAddRs", typeof(AccountAddRq))]
+    //[XmlElement("CustomerQueryRs", typeof(CustomerQueryRq))]
     public List<QBRequest> Requests { get; set; } = [];
 
     public List<XElement> ToXElement(QBContext? context)

02eddd93adce89032036edd230d369ad88d15686
travis@travestyofcode.com <travis@travestyofcode.com>
2025-08-10 13:53:02 -0400

    Adds / implements IToXElement and removes unneeded static extensions.

--- EmpirePump.Web/QBSDK/Filters/NameRangeFilter.cs
+++ EmpirePump.Web/QBSDK/Filters/NameRangeFilter.cs
@@ -1,9 +1,8 @@
-﻿using System.Runtime.CompilerServices;
-using System.Xml.Linq;
+﻿using System.Xml.Linq;
 
 namespace EmpirePump.Web.QBSDK;
 
-public class NameRangeFilter
+public class NameRangeFilter : IToXElement
 {
     public string? FromName { get; set; }
     public string? ToName { get; set; }
@@ -13,17 +12,5 @@ public class NameRangeFilter
         return new XElement(name)
             .AddElement(FromName)
             .AddElement(ToName);
-    }
-}
-
-internal static class NameRangeFilterExtensions
-{
-    public static XElement AddElement(this XElement element, NameRangeFilter? value, [CallerArgumentExpression(nameof(value))] string name = "")
-    {
-        if (value != null)
-        {
-            element.Add(value.ToXElement(name));
-        }
-        return element;
     }
 }
